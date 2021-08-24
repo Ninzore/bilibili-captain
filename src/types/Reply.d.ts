6ed1f8542c3c5f2d1c57bf9a65b746c785239429
Ninzore <33434617+Ninzore@users.noreply.github.com>
2021-08-24 17:56:47 +0100

    Change: Ctype -> Btype

--- src/types/Reply.d.ts
+++ src/types/Reply.d.ts
@@ -1,12 +1,5 @@
 import {Member, ContentMember} from "./User";
 
-export type Ctype = 
-1 | 2 | 4 | 5 | 6 |
-6 | 7 | 8 | 9 | 10 |
-11 | 12 | 13 | 14 | 15 |
-16 | 17 |18 | 19 | 20 |
-21 | 22 | 33
-
 declare const enum SortBy {
     time,
     like,
