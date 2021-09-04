bf7b8e4fb60ce9319d7300746c7b52e264dc2323
Ninzore <33434617+Ninzore@users.noreply.github.com>
2021-09-04 16:22:10 +0100

    Chore: rename file

--- src/Comment.ts
+++ src/Comment.ts
@@ -2,7 +2,7 @@ import * as querystring from "query-string";
 import {BiliCredential} from "./BiliCredential";
 import {Request} from "./Request";
 import {Btype} from "./types/Common";
-import {AddResponse, SortBy, ListResponse, CommonResponse} from "./types/Reply";
+import {AddResponse, SortBy, ListResponse, CommonResponse} from "./types/Comment";
 
 export class Comment {
     private credential: BiliCredential;
@@ -12,13 +12,40 @@ export class Comment {
     }
 
     /**
-     * 发送回复，如果回复另一条回复则需要parent和root
+     * 发送评论
      * @param oid 评论区id
      * @param message 回复内容
      * @param type 评论区类型
      * @returns 
      */
-     async add(oid: string, message: string, type: Btype, root?: number, parent?: number): Promise<AddResponse> {
+    async add(oid: string, type: Btype, message: string): Promise<AddResponse> {
+        let payload = {
+            oid,
+            message,
+            type,
+            plat: 1,
+            ordering: "heat",
+            jsonp: "jsonp",
+            csrf: this.credential.csfr,
+        }
+
+        return Request.post(
+            "https://api.bilibili.com/x/v2/reply/add",
+            querystring.stringify(payload),
+            this.credential
+        );
+    }
+
+    /**
+     * 发送回复
+     * @param oid 评论区id
+     * @param type 评论区类型
+     * @param root 回复串根
+     * @param parent 想要回复的那条回复
+     * @param message 回复内容
+     * @returns 
+     */
+     async reply(oid: string, type: Btype, root: number, parent: number, message: string): Promise<AddResponse> {
         let payload = {
             oid,
             message,
@@ -29,10 +56,6 @@ export class Comment {
             ordering: "heat",
             jsonp: "jsonp",
             csrf: this.credential.csfr,
-        }
-        if (!root && parent) {
-            delete payload.root;
-            delete payload.parent;
         }
 
         return Request.post(
@@ -49,7 +72,7 @@ export class Comment {
      * @param type 评论区类型
      * @returns 
      */
-     async delete(oid: string, reply_id: string, type: Btype): Promise<CommonResponse> {
+    async delete(oid: string, reply_id: string, type: Btype): Promise<CommonResponse> {
         return Request.post(
             "https://api.bilibili.com/x/v2/reply/del",
             querystring.stringify({
@@ -70,7 +93,7 @@ export class Comment {
      * @param type 评论区类型
      * @returns 
      */
-     async top(oid: string, reply_id: string, type: Btype, action: true): Promise<CommonResponse> {
+    async top(oid: string, reply_id: string, type: Btype, action: true): Promise<CommonResponse> {
         return Request.post(
             "https://api.bilibili.com/x/v2/reply/top",
             querystring.stringify({
@@ -92,7 +115,7 @@ export class Comment {
      * @param page_num 评论区页数
      * @returns 
      */
-     async list(oid: string, type: Btype, page_num = 0, sort = SortBy.like): Promise<ListResponse> {
+    async list(oid: string, type: Btype, page_num = 0, sort = SortBy.like): Promise<ListResponse> {
         return Request.get(
             "https://api.bilibili.com/x/v2/reply",
             {
@@ -115,7 +138,7 @@ export class Comment {
      * @param type 	评论区类型
      * @returns 
      */
-     async thumb(oid: string, rpid: string, type: Btype, action = true): Promise<CommonResponse> {
+    async thumb(oid: string, rpid: string, type: Btype, action = true): Promise<CommonResponse> {
         return Request.get(
             "https://api.bilibili.com/x/v2/reply/action",
             {
