4049e43229adfd9f9feef87b1f96caa54e046380
Ninzore <33434617+Ninzore@users.noreply.github.com>
2021-09-17 15:11:54 +0100

    Feat: 操作和用户的关系

--- src/User.ts
+++ src/User.ts
@@ -1,7 +1,13 @@
+import {BiliCredential} from "./BiliCredential";
 import {Request} from "./Request";
 import {UserInfo, UserInfoFromSearch} from "./types/User";
 
 export class User {
+    private credential: BiliCredential;
+
+    constructor(credential: BiliCredential, public uid?: number) {
+        this.credential = credential;
+    }
 
     /**
      * 查看用户信息
@@ -43,17 +49,72 @@ export class User {
       * @param keyword 搜索关键字
       * @returns 
       */
-     static async searchUser(keyword: string): Promise<UserInfoFromSearch[]> {
+    static async searchUser(keyword: string): Promise<UserInfoFromSearch[]> {
         return Request.get(
             "https://app.bilibili.com/x/v2/search/type",
             {
-                "keyword": keyword,
-                "build" : 63800200,  // 实测其实写啥都可以...但是得有
-                "order" : "fans",
-                "type" : 2,
+                keyword,
+                build : 63800200,  // 实测其实写啥都可以...但是得有
+                order : "fans",
+                type : 2,
             }
         ).then(res => {
             return 'items' in res.data ? res.data.items : [];
         });
-     }
+    }
+
+    /**
+     * 操作和用户的关系
+     * @param act 操作码 
+     * 关注=1，取关=2，悄悄关注=3，取消悄悄关注=4，拉黑=5，取消拉黑=6，去除粉丝=7
+     * @param re_src 来源 空间：11，视频：14，文章：115，活动页面：222
+     * @param uid 用户uid
+     * @returns 
+     */
+    private async _modifyRelation(act: 1 | 2 | 3 | 4 | 5 | 6 | 7 = 1, re_src: 11 | 14 | 115 | 222 = 11, uid?: number): Promise<boolean> {
+        uid = uid ?? this.uid;
+        if (!uid) throw "需要用户uid";
+
+        return Request.get(
+            "https://api.bilibili.com/x/relation/modify",
+            {
+                fid: uid,
+                act: act ? 1 : 2,
+                re_src,
+                csrf: this.credential.csfr
+            }
+        ).then(res => {return res.code === 0});
+    }
+
+    /**
+     * 关注/取关用户
+     * @param act 操作码，关注=1，取关=2，悄悄关注=3，取消悄悄关注=4
+     * @param re_src 来源， 空间：11，视频：14，文章：115，活动页面：222
+     * @param uid 用户uid
+     * @returns 
+     */
+    async follow(act: 1 | 2 | 3 | 4 = 1, re_src: 11 | 14 | 115 | 222 = 11, uid?: number): Promise<boolean> {
+        return this._modifyRelation(act, re_src, uid);
+    }
+
+    /**
+     * 加入，取消黑名单
+     * @param act 操作码，加入黑名单=5，取消黑名单=6
+     * @param re_src 来源， 空间：11，视频：14，文章：115，活动页面：222
+     * @param uid 用户uid
+     * @returns 
+     */
+    async block(act: 5 | 6 = 5, re_src: 11 | 14 | 115 | 222 = 11, uid?: number): Promise<boolean> {
+        return this._modifyRelation(act, re_src, uid);
+    }
+
+    /**
+     * 删除粉丝
+     * @param re_src 来源， 空间：11，视频：14，文章：115，活动页面：222
+     * @param uid 用户uid
+     * @returns 
+     */
+     async rmFan(re_src: 11 | 14 | 115 | 222 = 11, uid?: number): Promise<boolean> {
+        return this._modifyRelation(7, re_src, uid);
+    }
 }
