98e6151c7f9cbf293aa9e81a342bc898b79fb26f
Ninzore <33434617+Ninzore@users.noreply.github.com>
2021-10-06 22:23:11 +0100

    Style

--- src/request.ts
+++ src/request.ts
@@ -7,15 +7,15 @@ export class Request {
     static user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36";
     static referer = "https://www.bilibili.com";
     
-    static async request(url: string, method: Method, params: Object = {}, data?: string | Object | FormData, credential?: BiliCredential): Promise<any> {
-        let headers = {
+    static async request(url: string, method: Method, params: Object = {}, 
+    data?: string | Object | FormData, credential?: BiliCredential): Promise<any> {
+        const headers = {
             "User-Agent": this.user_agent,
             "Referer": this.referer,
-            "Cookie": "",
-            "Content-Type": "application/x-www-form-urlencoded"
+            "Cookie": credential ? credential.cookie_str : "",
+            "Content-Type": data instanceof FormData
+                ? data.getHeaders()["content-type"] : "application/x-www-form-urlencoded"
         }
-        if (credential) headers["Cookie"] = credential.cookie_str;
-        if (data instanceof FormData) headers["Content-Type"] = data.getHeaders()["content-type"];
 
         return axios(url, {
             method: method,
