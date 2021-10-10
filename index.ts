15d730eaa93d336f191efb9a996d6799fe0c1dfe
Ninzore <33434617+Ninzore@users.noreply.github.com>
2021-10-10 21:54:50 +0100

    bump to 0.5.0

---
+++ index.ts
@@ -0,0 +1,17 @@
+import {Live} from "./src/live";
+import {User} from "./src/user";
+import {Login} from "./src/login";
+import {Video} from "./src/video";
+import {Comment} from "./src/comment";
+import {Dynamic} from "./src/dynamic";
+import {Request} from "./src/request";
+import {Message} from "./src/message";
+import {BiliCredential} from "./src/biliCredential";
+
+export default {
+    Live, User, Login, Video, Comment, Dynamic, Request, Message, BiliCredential
+}
+
+export {
+    Live, User, Login, Video, Comment, Dynamic, Request, Message, BiliCredential
+};
