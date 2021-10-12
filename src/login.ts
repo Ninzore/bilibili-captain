6981b712fde92a1812e29d5ce558f5dbabfca7d3
Ninzore <33434617+Ninzore@users.noreply.github.com>
2021-10-12 19:18:13 +0100

    Feat: 轮询确认是否成功扫码
    Chore: 不在依赖 qrcode-terminal
    Chore: 添加依赖 qrcode

--- src/login.ts
+++ src/login.ts
@@ -1,8 +1,8 @@
 import {URL} from "url";
-import * as qrcode from "qrcode-terminal";
+import * as qrcode from "qrcode";
 import {Request} from "./request";
 import {BiliCredential} from "./biliCredential";
-import {GetQRResp} from "./types/login";
+import {GetQRResp, GetLoginInfoResp} from "./types/login";
 
 /**
  * 登录
@@ -25,11 +25,34 @@ export class Login {
      * @param oauthKey 扫码登录秘钥
      * @returns 
      */
-    private static async _getQRloginInfo(oauthKey: string): Promise<any> {
+    private static async _getQRloginInfo(oauthKey: string): Promise<GetLoginInfoResp> {
         return Request.post(
             "http://passport.bilibili.com/qrcode/getLoginInfo",
             {oauthKey}
-        );
+        )
+        .then(res => res.data)
+        .catch(err => {
+            if (err.data) return err;
+            else throw err;
+        });
+    }
+
+    /**
+     * 输出QRcode渲染结果
+     * @param output 选择输出形式
+     */
+    static async renderQR(text: string, output: "buffer"): Promise<Buffer>
+    static async renderQR(text: string, output: "string"): Promise<string>
+    static async renderQR(text: string, output: "terminal"): Promise<void>
+    static async renderQR(text: string, output: "terminal" | "string" | "buffer"): Promise<void | string | Buffer> {
+        switch (output) {
+            case "string": return qrcode.toString(text);
+            case "buffer": return qrcode.toBuffer(text);
+            case "terminal": {
+                console.log(await qrcode.toString(text));
+                return;
+            }
+        }
     }
 
     /**
@@ -38,28 +61,42 @@ export class Login {
      */
     static async loginQR(): Promise<BiliCredential> {
         const qr_info = await this._getQRcode();
-        console.log("打开手机进行扫码，请在10秒内完成", qrcode.generate(qr_info.url, {small: true}));
-        await new Promise(resolve => {
-            setTimeout(() => {
-                resolve("ok");
-            }, 10000);
-        });
+        console.log("打开手机进行扫码，请在3分钟内完成");
+        Login.renderQR(qr_info.url, "terminal");
 
-        const res = await this._getQRloginInfo(qr_info.oauthKey);
-        switch (res.data) {
-            case -1: throw "秘钥错误";
-            case -2: throw "密钥超时";
-            case -4: throw "未扫描";
-            case -5: throw "未确认";
-            case 0: {
-                const params = new URL(res.data.url).searchParams;
-                const sessdata = params.get("SESSDATA");
-                const bili_jct = params.get("bili_jct");
-                if (!sessdata) throw "未取得SESSDATA";
-                if (!bili_jct) throw "未取得bili_jct";
-                return new BiliCredential(sessdata, bili_jct);
-            };
-            default: throw res.data;
-        }
+        return new Promise(resolve => {
+            let last_stat = -1;
+            let clear: ReturnType<typeof setTimeout>;
+
+            const polling = setInterval(async () => {
+                const res = await this._getQRloginInfo(qr_info.oauthKey);
+                
+                if (res.url) {
+                    last_stat = 0;
+                    clearInterval(polling);
+                    clearTimeout(clear);
+                    const params = new URL(res.url).searchParams;
+                    const sessdata = params.get("SESSDATA");
+                    const bili_jct = params.get("bili_jct");
+                    if (!sessdata) throw "未取得SESSDATA";
+                    if (!bili_jct) throw "未取得bili_jct";
+                    console.log("已完成扫码登录");
+                    resolve(new BiliCredential(sessdata, bili_jct));
+                }
+                else last_stat = res.data;
+            }, 3000);
+
+            clear = setTimeout(polling => {
+                if (last_stat !== 0) clearInterval(polling);
+                switch (last_stat) {
+                    case 0: return;
+                    case -1: throw "秘钥错误";
+                    case -2: throw "密钥超时";
+                    case -4: throw "未扫描";
+                    case -5: throw "未确认";
+                    default: throw "未知错误";
+                }
+            }, 180000, polling);
+        });
     }
 }
