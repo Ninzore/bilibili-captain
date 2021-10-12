a6f26fa4ab505cfb910fe7010d2bf6c6d7946bad
Ninzore <33434617+Ninzore@users.noreply.github.com>
2021-10-12 22:29:39 +0100

    Refactor: simplify logic

--- src/login.ts
+++ src/login.ts
@@ -65,16 +65,13 @@ export class Login {
         Login.renderQR(qr_info.url, "terminal");
 
         return new Promise(resolve => {
-            let last_stat = -1;
-            let clear: ReturnType<typeof setTimeout>;
+            let count_down = 60;
 
-            const polling = setInterval(async () => {
+            const polling = setTimeout(async (count_down) => {
                 const res = await this._getQRloginInfo(qr_info.oauthKey);
                 
                 if (res.url) {
-                    last_stat = 0;
                     clearInterval(polling);
-                    clearTimeout(clear);
                     const params = new URL(res.url).searchParams;
                     const sessdata = params.get("SESSDATA");
                     const bili_jct = params.get("bili_jct");
@@ -83,20 +80,22 @@ export class Login {
                     console.log("已完成扫码登录");
                     resolve(new BiliCredential(sessdata, bili_jct));
                 }
-                else last_stat = res.data;
-            }, 3000);
+                else {
+                    count_down --;
 
-            clear = setTimeout(polling => {
-                if (last_stat !== 0) clearInterval(polling);
-                switch (last_stat) {
-                    case 0: return;
-                    case -1: throw "秘钥错误";
-                    case -2: throw "密钥超时";
-                    case -4: throw "未扫描";
-                    case -5: throw "未确认";
-                    default: throw "未知错误";
+                    if (count_down > 0) polling.refresh();
+                    else {
+                        switch (res.data) {
+                            case -1: throw "秘钥错误";
+                            case -2: throw "密钥超时";
+                            case -4: throw "未扫描";
+                            case -5: throw "未确认";
+                            default: throw "未知错误";
+                        }
+                    }
                 }
-            }, 180000, polling);
+                
+            }, 3000, count_down);
         });
     }
 }
