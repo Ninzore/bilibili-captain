d7d82601e08d6cb3ab3adc80def4dbab6df9848a
Ninzore <33434617+Ninzore@users.noreply.github.com>
2023-12-22 17:18:04 +0000

    Fix: opposite result from devIdVerify

--- src/sign.ts
+++ src/sign.ts
@@ -1,5 +1,5 @@
 export function devIdVerify(devId: string): boolean {
-    return !/[\dA-F]{8}-[\dA-F]{4}-4[\dA-F]{3}-[\dA-F]{4}-[\dA-F]{12}/.test(devId);
+    return /[\dA-F]{8}-[\dA-F]{4}-4[\dA-F]{3}-[\dA-F]{4}-[\dA-F]{12}/.test(devId);
 }
 
 export function devIdGen(): string {
