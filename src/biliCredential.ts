c54835c1bd9af036f1992ee2f1085c9f849126a6
Ninzore <33434617+Ninzore@users.noreply.github.com>
2024-07-22 07:45:25 +0100

    [BREAKING] Feat: new credential initialisation

--- src/biliCredential.ts
+++ src/biliCredential.ts
@@ -1,14 +1,20 @@
 import { User } from "./user";
 import { UserInfo } from "./types/user";
-import { devIdGen, devIdVerify } from "./sign";
+import { devIdGen, devIdVerify, genBuvidFp, genUuid, getFingerSpi } from "./sign";
 
 /**
  * 保存各种凭据
  */
 export class BiliCredential {
-    public cookie: {
+    // 参考https://github.com/SocialSisterYi/bilibili-API-collect/issues/933
+    public cookies: {
         SESSDATA: string;
         bili_jct: string;
+        buvid_fp: string;
+        buvid3: string;
+        buvid4: string;
+        b_nut: string; // 时间戳
+        _uuid: string;
     };
 
     public cookieStr: string;
@@ -24,24 +30,40 @@ export class BiliCredential {
     /**
      * @param SESSDATA 必要
      * @param biliJct 必要
-     * @param extra uid: 用户uid; dev_id: device id，不传的话会自动生成
+     * @param extra? 可选
      */
     constructor(SESSDATA: string, biliJct: string,
         extra?: {
-            uid?: number, refreshToken?: string,
-            devId?: string, timestamp?: number, dedeUserIdCkMd5?: string
+            uid?: number,
+            refreshToken?: string,
+            devId?: string,
+            buvid3?: string,
+            buvid4?: string,
+            b_lsid?: string,
+            buvid_fp?: string,
+            timestamp?: number,
+            dedeUserIdCkMd5?: string
         },
     ) {
-        this.cookie = { SESSDATA, bili_jct: biliJct };
+        this.timestamp = extra?.timestamp ? extra?.timestamp : Date.now();
+        this.cookies = {
+            SESSDATA,
+            bili_jct: biliJct,
+            buvid_fp: "",
+            buvid3: "",
+            buvid4: "",
+            b_nut: this.timestamp.toString(),
+            _uuid: genUuid(),
+        };
         this.csfr = biliJct;
         this.uid = extra?.uid ? extra?.uid : 0;
         this.refreshToken = extra?.refreshToken ? extra?.refreshToken : "";
-        this.timestamp = extra?.timestamp ? extra?.timestamp : Date.now();
+
         this.dedeUserIdCkMd5 = extra?.dedeUserIdCkMd5 ? extra?.dedeUserIdCkMd5 : "";
         this.valid = false;
 
         const tmp: String[] = [];
-        for (const [k, v] of Object.entries(this.cookie)) {
+        for (const [k, v] of Object.entries(this.cookies)) {
             tmp.push(`${k}=${v}`);
         }
         this.cookieStr = tmp.join("; ");
@@ -51,9 +73,24 @@ export class BiliCredential {
                 throw new Error("dev_id 不符合规范，参考https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/message/private_msg.md");
             }
         }
-        else {
-            this.devId = devIdGen();
+        else this.devId = devIdGen();
+    }
+
+    async init() {
+        const { b_3, b_4 } = await getFingerSpi();
+        this.cookies.buvid3 = b_3;
+        this.cookies.buvid4 = b_4;
+        this.cookies.b_nut = this.timestamp.toString();
+        this.cookies.buvid_fp = genBuvidFp("Hello from BiliBili-Captain at" + this.timestamp);
+
+        const tmp: String[] = [];
+        for (const [k, v] of Object.entries(this.cookies)) {
+            tmp.push(`${k}=${v}`);
         }
+        this.cookieStr = tmp.join("; ");
+
+        await this.validCredit();
+        await this.loadInfo();
     }
 
     async validCredit() {
